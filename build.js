b1c1e51684d5fe8f82786528a3a6bb16ee9f976f
Kerri Shotts <shotts@adobe.com>
2018-02-01 14:45:03 -0500

    rethink build process

--- build.js
+++ build.js
@@ -26,13 +26,23 @@ function copyDocsFromModules() {
         if (module === "core") {
             cpTo = DOCS_PATH;
         } else {
-            cpTo = path.join(DOCS_PATH, "zz_ecosystem", "libraries", module);
+            cpTo = path.join(DOCS_PATH, "ecosystem", "libraries", module);
         }
         shell.mkdir("-p", cpTo);
         shell.cp('-r', path.join(cpFrom, '*'), cpTo);
         shell.cp(path.join(cpFrom, '..', 'README.md'), path.join(cpTo, 'README.md'));
     });
+
+    // some files need special handling
+    ['ACKNOWLEDGEMENTS.md', 'CONTACT.md', 'CODE_OF_CONDUCT.md', 'CONTRIBUTING.md'].forEach( file => {
+        const cpFrom = path.join(TWIST_MODULES_PATH, 'core');
+        const cpTo = DOCS_PATH;
+        shell.cp(path.join(cpFrom, file), cpTo);
+    });
+
+    shell.mv(path.join(DOCS_PATH, 'README.md'), path.join('.', 'site', 'README.md'));
 }
+
 
 function fixDocs() {
     shell.find(path.join(DOCS_PATH, '**', '*.md')).forEach(file => {
@@ -40,13 +50,6 @@ function fixDocs() {
         shell.sed('-i', /\{\{/g, '{ {', file);
         shell.sed('-i', /\}\}/g, '} }', file);
     });
-}
-
-function customSort(a, b) {
-    if (path.basename(a) === 'index.md') {
-        return -1;
-    }
-    return a < b ? -1 : a > b ? 1 : 0;
 }
 
 function buildTree(curPath) {
@@ -57,7 +60,7 @@ function buildTree(curPath) {
                 case '':
                     const subtree = buildTree(path.join(curPath, file));
                     if (subtree.length > 0) {
-                        return { [file]: [path.join(curPath, file), ...buildTree(path.join(curPath, file))] };
+                        return { [file]: [path.join(curPath, file), ...subtree] };
                     }
                     break;
                 case '.md':
@@ -83,6 +86,7 @@ function flatten(...items) {
 function titleCase(str) {
     return str
         .replace(/^zz\_/, '')
+        .replace(/^\d*\_/, '')
         .split(/[-|\s]/)
         .map(word => word[0].toUpperCase() + word.substr(1))
         .join(' ')
@@ -90,51 +94,36 @@ function titleCase(str) {
         .replace(/\>/g, '&gt;');
 }
 
-function getTitle(file) {
-    if (path.basename(file) === 'README.md') {
-        return 'README';
-    }
-    if (path.basename(file) === 'index.md') {
-        return 'Index';
-    }
-    const title =
-        shell
-            .cat(path.join(DOCS_PATH, file))
-            .split('\n')
-            .reduce((title, line) => {
-                if (line.substr(0, 2) === '# ' && !title) {
-                    title = line.substr(2);
-                }
-                return title;
-            }, '') || path.basename(file, '.md');
-
-    return titleCase(title);
-}
 
 function buildSummary() {
-    const tree = buildTree(DOCS_PATH);
+
+    let libraries = shell.find(path.join(DOCS_PATH, 'ecosystem', 'libraries', '**', 'index.md'));
+    if (libraries && !libraries.stderr) {
+        libraries = libraries.map(file => 
+            shell.cat(file).replace(/\.\//g, path.dirname(file).replace('site/', './') + path.sep)
+        )
+    } else {
+        libraries = [];
+    }
+
+    const sections = [
+        '# Summary',
+        shell.cat(path.join(DOCS_PATH, 'index.md')).replace(/\.\//g, './pages/'),
+        '',
+        '# Ecosystem',
+        '## Libraries',
+        ...libraries,
+        '## Community',
+        '* [Acknowledgements](./pages/ACKNOWLEDGEMENTS.md)',
+        '* [Contributing](./pages/CONTRIBUTING.md)',
+        '* [Code of Conduct](./pages/CODE_OF_CONDUCT.md)',
+        '* [Contact](./pages/CONTACT.md)'
+    ];
+
     fs.writeFileSync(
         path.join('.', 'site', 'SUMMARY.md'),
-        flatten(tree)
-            .map(file => file.replace('site/pages/', ''))
-            .filter(file => path.basename(file) !== 'index.md')
-            .reduce(
-                (summary, file) => {
-                    const indent = Array.from({ length: file.split(path.sep).length }).join('    ');
-                    switch (path.extname(file)) {
-                        case '.md':
-                            summary.push(`${indent}* [${getTitle(file)}](./pages/${file})`);
-                            break;
-                        case '':
-                            summary.push(`${indent}* ${titleCase(path.basename(file))}`);
-                            break;
-                    }
-                    return summary;
-                },
-                ['# Summary']
-            )
-            .join('\n'),
-        { encoding: 'utf8' }
+        sections.join("\n"),
+        {encoding: 'utf8'}
     );
 }
 
